66f20406165ac8e055c26096ad2d31eca9748368
Rafik Parsyan <rafik_parsyan@epam.com>
2024-01-22 03:01:52 +0400

    minor change

--- src/database/db.py
+++ src/database/db.py
@@ -15,7 +15,6 @@ class Database:
         row = URLS(original_url=long_url, short_url=short_url)
         session.add(row)
         session.commit()
-        session.close()
 
     def delete(self, url):
         session = self.create_session()
@@ -25,19 +24,16 @@ class Database:
         if record:
             session.delete(record[0])
             session.commit()
-        session.close()
 
     def get_long_url(self, url):
         session = self.create_session()
         query = select(URLS).where(URLS.short_url == url)
         result = session.execute(query)
         record = result.fetchone()
-        session.close()
         return record[0].original_url if record else None
 
     def get_short_url(self, url):
         session = self.create_session()
         query = select(URLS).where(URLS.original_url == url)
         result = session.execute(query)
-        session.close()
         return result.fetchone()
